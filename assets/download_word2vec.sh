f8109977e305516255898ad498d6b64ff836c049
Biagio Brattoli <biagio.brattoli@gmail.com>
2020-03-03 16:05:54 +0100

    first

--- assets/download_word2vec.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-wget https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz -O /workplace/GoogleNews-vectors-negative300.bin.gz
-gunzip -c /workplace/GoogleNews-vectors-negative300.bin.gz > /workplace/GoogleNews-vectors-negative300.bin
